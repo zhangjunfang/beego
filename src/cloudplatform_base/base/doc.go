625b1e31c6248c29b1f29e6e71d98c21d77b863c
zhangjunfang <zhangjunfang0505@163.com>
2015-10-19 11:34:08 +0800

    20151019

---
+++ src/cloudplatform_base/base/doc.go
@@ -0,0 +1,6 @@
+// cloudplatform_base project doc.go
+
+/*
+cloudplatform_base document
+*/
+package cloudplatform_base
---
+++ src/cloudplatform_cp/main.go
@@ -0,0 +1,10 @@
+// 1.实现云平台与慧修车服务端以及宇通之间通信协议 main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+}
